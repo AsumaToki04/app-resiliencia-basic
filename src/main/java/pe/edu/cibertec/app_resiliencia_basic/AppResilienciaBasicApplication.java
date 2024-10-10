d8c22f72127ad5555726842ba3d2f0846c5f27e9
AsumaToki04 <megamanzxu@hotmail.com>
2024-10-10 09:28:17 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_resiliencia_basic/AppResilienciaBasicApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_resiliencia_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppResilienciaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppResilienciaBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_resiliencia_basic/AppResilienciaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_resiliencia_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppResilienciaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
